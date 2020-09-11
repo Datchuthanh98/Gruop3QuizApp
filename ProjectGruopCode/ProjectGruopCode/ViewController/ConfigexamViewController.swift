f27b5f1641034301f9895a30e42ec08e3d587ab7
Chu Thanh Dat <chuthanhdat@V000061s-Mac-mini.local>
2020-09-11 12:11:31 +0700

    finalv2

--- ProjectGruopCode/ProjectGruopCode/ViewController/ConfigexamViewController.swift
+++ ProjectGruopCode/ProjectGruopCode/ViewController/ConfigexamViewController.swift
@@ -21,7 +21,7 @@ class ConfigexamViewController: UIViewController {
     @IBOutlet weak var lblTime: UILabel!
     @IBOutlet weak var lblQuestion: UILabel!
     
-
+    
     @IBOutlet weak var btnSave: UIButton!
     
     @IBOutlet weak var btnEdit: UIButton!
@@ -49,7 +49,7 @@ class ConfigexamViewController: UIViewController {
         lblTime.text = String(timeDefaut)
         inputQuestion.text = String(questionDefaut)
         lblQuestion.text = String(questionDefaut)
-  
+        
         inputQuestion.isHidden = true
         inputTime.isHidden = true
         btnSave.isHidden = true
@@ -57,70 +57,91 @@ class ConfigexamViewController: UIViewController {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapOnScreen))
         view.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
     @objc func tapOnScreen() {
         inputTime.resignFirstResponder()
         inputQuestion.resignFirstResponder()
     }
-
+    
     @IBAction func clickSave(_ sender: Any) {
-                     updateToFirebase()
-                self.lblQuestion.text = self.inputQuestion.text
-                self.lblTime.text  = self.inputTime.text
-                       inputTime.isHidden = true
-                       lblTime.isHidden = false
-                inputQuestion.isHidden = true
-                lblQuestion.isHidden = false
-                       let icon = UIImage(systemName: "doc.text.fill")
-        btnSave.isHidden = true
-    }
-
-
    
-
-  
+        
+        if(inputQuestion.text == String(questionDefaut) && inputTime.text == String(timeDefaut)){
+            print("do nothing")
+        }else if (inputQuestion.text == "" || inputTime.text == "" ) {
+            let alert = UIAlertController(title: "INVALID INPUT", message: "Required fields can not be blank! Please try again.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }else if (Int(inputTime.text!) ?? 0 > 90 || Int(inputTime.text!) ?? 0 <= 0) {
+            let alert = UIAlertController(title: "INVALID INPUT", message: "Required fields time can  not over limit! Please try again.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if ( Int(inputQuestion.text!) ?? 0 > 20 || Int(inputQuestion.text!) ?? 0 <= 0 ) {
+            let alert = UIAlertController(title: "INVALID INPUT", message: "Required fields  number question can  not over limit! Please try again.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+                 updateToFirebase()
+            self.lblQuestion.text = self.inputQuestion.text
+            self.lblTime.text  = self.inputTime.text
+            inputTime.isHidden = true
+            lblTime.isHidden = false
+            inputQuestion.isHidden = true
+            lblQuestion.isHidden = false
+            let icon = UIImage(systemName: "doc.text.fill")
+            btnSave.isHidden = true
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
     @IBAction func btnCancel(_ sender: Any) {
-                self.navigationController?.popViewController(animated: true)
+        self.navigationController?.popViewController(animated: true)
     }
     func updateToFirebase(){
-         let setting = [
+        let setting = [
             "time" : Int(self.inputTime.text!)! ,
             "question" : Int(self.inputQuestion.text!)!
-                ] as [String : Any]
-                ref.child("setting").child("1").setValue(setting,withCompletionBlock: { error , ref in
-                    if error == nil {
-                        self.dismiss(animated: true, completion: nil)
-                    }else{
-                    }
-                } )
-
-         
-
+            ] as [String : Any]
+        ref.child("setting").child("1").setValue(setting,withCompletionBlock: { error , ref in
+            if error == nil {
+                self.dismiss(animated: true, completion: nil)
+            }else{
+            }
+        } )
+        
+        
+        
         
         UserDefaults.standard.set(Int(inputTime.text!)!, forKey: "Time")
         UserDefaults.standard.set(Int(inputQuestion.text!)!, forKey: "NumbersQ")
         
         
-               let alertController = UIAlertController(title: "Update sucessfully", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Update sucessfully", message: nil, preferredStyle: .alert)
         self.present(alertController, animated: true, completion: nil)
-     }
-     func setProfile(){
-         if(self.option == 1){
-                   self.id = UserDefaults.standard.string(forKey: "idFB") ?? ""
-               } else {
-                   self.id = UserDefaults.standard.string(forKey: "idGG") ?? ""
-               }
-     }
+    }
+    func setProfile(){
+        if(self.option == 1){
+            self.id = UserDefaults.standard.string(forKey: "idFB") ?? ""
+        } else {
+            self.id = UserDefaults.standard.string(forKey: "idGG") ?? ""
+        }
+    }
     @IBAction func btnEdit(_ sender: Any) {
-                if(isEdit == false){
-                       lblTime.isHidden = true
-                      lblQuestion.isHidden = true
-                       inputTime.isHidden = false
-                     inputQuestion.isHidden = false
-                       let icon = UIImage(systemName: "doc.text")
-                       btnEdit.setImage(icon, for: .normal)
-                       btnSave.isHidden = false
-                       isEdit == true
-                   }
+        if(isEdit == false){
+            lblTime.isHidden = true
+            lblQuestion.isHidden = true
+            inputTime.isHidden = false
+            inputQuestion.isHidden = false
+            let icon = UIImage(systemName: "doc.text")
+            btnEdit.setImage(icon, for: .normal)
+            btnSave.isHidden = false
+            isEdit == true
+        }
     }
 }
